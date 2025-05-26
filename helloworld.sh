053c8bc9f33bfd88d3ea2d48a68db9184ca235e7
TAKE Takashi <nakamizo.takashi@gmail.com>
2025-05-26 22:56:27 +0900

    追加

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title HelloWorld
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description ハローワールドスクリプト
+
+echo "Hello World!"
